4066c49aba26e11fa957d554fb78eade5db71ee5
nossbigg <chenggibson@gmail.com>
2021-12-10 15:06:34 +1100

    Parse homepage to get top-levle category urls

--- src/catalog.rs
+++ src/catalog.rs
@@ -1,20 +1,38 @@
-const CHEMIST_WAREHOUSE_URL_HOMEPAGE: &str = "https://www.chemistwarehouse.com.au/";
+use regex::Regex;
+use scraper::{Html, Selector};
+
+const CHEMIST_WAREHOUSE_URL_HOMEPAGE: &str = "https://www.chemistwarehouse.com.au";
 
 #[tokio::main]
 pub async fn handle_catalogs() {
-    let homepage_request = get_homepage();
-    let body: String = match homepage_request.await {
+    let homepage_request = get_page(CHEMIST_WAREHOUSE_URL_HOMEPAGE);
+    let html: String = match homepage_request.await {
         Ok(v) => v,
-        _ => String::from(""),
+        _ => {
+            println!("{}", "Error retrieving homepage.");
+            return;
+        }
     };
 
-    println!("{}", body);
+    let top_level_category_urls = parse_homepage(html);
 }
 
-async fn get_homepage() -> Result<String, reqwest::Error> {
-    let result = reqwest::get(CHEMIST_WAREHOUSE_URL_HOMEPAGE)
-        .await?
-        .text()
-        .await?;
+async fn get_page(url: &str) -> Result<String, reqwest::Error> {
+    let result = reqwest::get(url).await?.text().await?;
     return Ok(result);
+}
+
+fn parse_homepage(html: String) -> Vec<String> {
+    let category_url_matcher = Regex::new(r"shop-online").unwrap();
+    let document = Html::parse_document(&html);
+
+    let selector = Selector::parse("ul.menu-items > li > a").unwrap();
+
+    let categories_url = document
+        .select(&selector)
+        .map(|e| e.value().attr("href").unwrap().replace("\"", ""))
+        .filter(|href| category_url_matcher.is_match(&href))
+        .collect::<Vec<String>>();
+
+    return categories_url;
 }
