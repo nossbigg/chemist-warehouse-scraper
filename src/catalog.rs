fd6a5dd67810913732bcb611e72cf97a23232d80
nossbigg <chenggibson@gmail.com>
2021-12-19 21:02:06 +1100

    Recursively parse through all categories

--- src/catalog.rs
+++ src/catalog.rs
@@ -1,7 +1,20 @@
 use regex::Regex;
 use scraper::{Html, Selector};
+use std::fmt;
 
 const CHEMIST_WAREHOUSE_URL_HOMEPAGE: &str = "https://www.chemistwarehouse.com.au";
+
+#[derive(Clone)]
+struct MyNode {
+    from: String,
+    to: String,
+}
+
+impl fmt::Debug for MyNode {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> fmt::Result {
+        write!(f, "{} -> {}", self.from, self.to)
+    }
+}
 
 #[tokio::main]
 pub async fn handle_catalogs() {
@@ -15,11 +28,21 @@ pub async fn handle_catalogs() {
     };
 
     let top_level_category_urls = parse_homepage(html);
+    let top_level_category_ids = get_top_level_category_ids(top_level_category_urls);
 
-    let mut category_ids: Vec<String> = Vec::new();
-    let category_id_matcher = Regex::new(r"shop-online/(\d+)").unwrap();
-    for category_url in top_level_category_urls {
-        parse_category_page(category_url, &category_id_matcher, &mut category_ids).await
+    let mut all_nodes: Vec<MyNode> = Vec::new();
+    let mut current_nodes: Vec<MyNode> = top_level_category_ids;
+
+    while current_nodes.len() > 0 {
+        let mut next_nodes: Vec<MyNode> = Vec::new();
+
+        for entry in current_nodes {
+            let mut result = parse_category_page(&entry.to).await;
+            next_nodes.append(&mut result);
+        }
+
+        current_nodes = next_nodes.to_owned();
+        all_nodes.append(&mut next_nodes);
     }
 }
 
@@ -47,40 +70,57 @@ fn parse_homepage(html: String) -> Vec<String> {
     return categories_url;
 }
 
-async fn parse_category_page(
-    category_url: String,
-    category_id_matcher: &Regex,
-    category_ids: &mut Vec<String>,
-) {
-    if !category_id_matcher.is_match(&category_url) {
-        return;
-    }
-    let captures = category_id_matcher
-        .captures_iter(&category_url)
-        .nth(0)
-        .unwrap();
-    let category_id = &captures[1];
-    category_ids.push(category_id.into());
+fn get_top_level_category_ids(category_urls: Vec<String>) -> Vec<MyNode> {
+    let category_id_matcher = Regex::new(r"shop-online/(\d+)").unwrap();
+    let mut category_ids: Vec<MyNode> = Vec::new();
 
-    let api_category_url = make_search_api_url(category_id.into(), "0");
+    for category_url in category_urls {
+        if !category_id_matcher.is_match(&category_url) {
+            continue;
+        }
+        let captures = category_id_matcher
+            .captures_iter(&category_url)
+            .nth(0)
+            .unwrap();
+        let category_id = &captures[1];
+
+        let entry = MyNode {
+            from: "".to_string(),
+            to: category_id.into(),
+        };
+        category_ids.push(entry);
+    }
+
+    return category_ids;
+}
+
+async fn parse_category_page(category_id: &str) -> Vec<MyNode> {
+    let api_category_url = make_search_api_url(category_id, "0");
     let api_request = get_page(&api_category_url);
     let response = match api_request.await {
         Ok(v) => v,
         _ => {
-            println!("Error retrieving category url. {}", category_url);
-            return;
+            println!("Error retrieving category id. {}", category_id);
+            return Vec::new();
         }
     };
 
     let json = parse_json(&response);
     let filters = &json["universes"]["universe"][0]["facetmap"][0]["filter"][0]["filtersection"];
 
-    let mut child_category_ids: Vec<String> = Vec::new();
+    let mut child_category_ids: Vec<MyNode> = Vec::new();
     for filter in filters.members() {
         let child_category_id_value = &filter["value"]["value"];
         let child_category_id = get_category_id(child_category_id_value.as_str().unwrap());
-        child_category_ids.push(child_category_id)
+
+        let entry = MyNode {
+            from: category_id.into(),
+            to: child_category_id,
+        };
+        child_category_ids.push(entry)
     }
+
+    return child_category_ids;
 }
 
 fn get_category_id(value: &str) -> String {
