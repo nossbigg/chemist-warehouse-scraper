aac58bdf30db91b356bae6e8913b6dc97e26a01f
nossbigg <chenggibson@gmail.com>
2022-01-11 23:36:45 +1100

    Write outputs to /output

--- src/catalogs/write_csv.rs
+++ src/catalogs/write_csv.rs
@@ -1,10 +1,15 @@
 use super::structs::MyNode;
+use crate::common::utils::create_output_dir;
 use std::fs::File;
+use std::path::Path;
 
 pub fn write_csv(nodes: Vec<MyNode>) {
-    let filename = format!("catalogs.csv");
-    let file = File::create(filename).unwrap();
+    create_output_dir();
 
+    let filename = "catalogs.csv";
+    let target = Path::join(Path::new("output"), filename);
+
+    let file = File::create(target).unwrap();
     let mut wtr = csv::Writer::from_writer(file);
 
     for node in nodes {
--- src/common/utils.rs
+++ src/common/utils.rs
@@ -1,3 +1,6 @@
+use std::fs::create_dir_all;
+use std::path::Path;
+
 pub async fn get_page(url: &str) -> Result<String, reqwest::Error> {
     let result = reqwest::get(url).await?.text().await?;
     return Ok(result);
@@ -9,4 +12,8 @@ pub fn make_search_api_url(category_id: &str, index: &str) -> String {
 
 pub fn parse_json(value: &str) -> json::JsonValue {
     json::parse(&value).unwrap()
+}
+
+pub fn create_output_dir() {
+    create_dir_all(Path::new("output")).unwrap();
 }
